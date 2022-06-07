b96759a40fcdfad10734071776936514ace4dfa1
Jamie Snape <snape@me.com>
2022-06-07 19:50:58 -0400

    Follow REUSE recommendations

--- Johnson.hs
+++ Johnson.hs
@@ -1,5 +1,7 @@
 -- This file is part of "Loopless Functional Algorithms".
--- Copyright (c) 2005 Jamie Snape, Oxford University Computing Laboratory.
+--
+-- SPDX-FileCopyrightText: 2005 Jamie Snape, Oxford University Computing Laboratory
+-- SPDX-License-Identifier: Apache-2.0
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
